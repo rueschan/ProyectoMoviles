4a5a5fd785e794928875df692f2ffc09b9e6c0ce
rueschan <rescalante21@hotmail.com>
2018-02-28 10:59:13 -0600

    Initial commit

---
+++ app/src/main/java/mx/itesm/rueschan/moviles/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.itesm.rueschan.moviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
