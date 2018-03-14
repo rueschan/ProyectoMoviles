4003eb6db8601c05590ba9c315720c0949aa4ee4
irv0304 <A01371240@itesm.mx>
2018-03-13 21:06:06 -0600

    Implementación del Main, del closet

---
+++ app/src/main/java/mx/itesm/rueschan/moviles/ClosetFragment.java
@@ -0,0 +1,93 @@
+package mx.itesm.rueschan.moviles;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ClosetFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
+
+        ControllerAdapter adapter = new ControllerAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        int paddingInBetween = getResources().getDimensionPixelSize(R.dimen.sizes);
+        recyclerView.setPadding(paddingInBetween, paddingInBetween, paddingInBetween, paddingInBetween);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        public ImageView imageView;
+        public TextView textView;
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.closet_item, parent, false));
+            imageView = itemView.findViewById(R.id.title_img);
+            textView = (TextView) itemView.findViewById(R.id.ettitle);
+
+        }
+    }
+
+    public static class ControllerAdapter extends RecyclerView.Adapter<ViewHolder>{
+
+        private static final  int SIZE = 4;
+        private final String[] arrayTexto;
+        private final Drawable[] arrayPictures;
+
+        public ControllerAdapter(Context context){
+
+            Resources resources = context.getResources();
+            arrayTexto = resources.getStringArray(R.array.types);
+            TypedArray typedArray = resources.obtainTypedArray(R.array.menupictures);
+            arrayPictures = new Drawable[typedArray.length()];
+            for (int i = 0; i < arrayPictures.length; i++) {
+                arrayPictures[i] = typedArray.getDrawable(i);
+            }
+            typedArray.recycle();
+
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.imageView.setImageDrawable(arrayPictures[position%arrayPictures.length]);
+            holder.textView.setText(arrayTexto[position%arrayTexto.length]);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return SIZE;
+        }
+    }
+}
